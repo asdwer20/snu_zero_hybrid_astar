a3f595e8671e5f72eb2f0e42da0df4f896719815
asdwer20 <31360565+asdwer20@users.noreply.github.com>
2019-07-10 13:40:27 +0900

    Update astar_planner.cpp

--- astar_planner.cpp
+++ astar_planner.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 #include "ompl/base/spaces/ReedsSheppStateSpace.h"
-#icnlude "omple/geometric/SimpleSetup.h"
+#include "ompl/geometric/SimpleSetup.h"
 #include "ompl/geometric/ScopedState.h"
 #include "ompl/geometric/planner.h"
 #include "carsetupcomhandle.h"
@@ -27,9 +27,28 @@ int main(int argc, char **argv){
     //Date input from Rviz
     CarSetupComHandle com_handle = CarSetupComHandle(argc, argv, node_name);
     com_handle.SimpleSetup();
-    
+
+    int seq = 0;
     while(ros::ok()){
         //Check if the message was updated
+        if(seq < CarSetupComHandle::GetLatestSeq()){
+            //Clear the previous state space, update seq, acquire map
+            state_space.clear();
+            seq = CarSetupComHandle::GetLatestSeq();
+            nav_msgs::OccupancyGridConstPtr input_map = CarSetupComHandle::GetMap(map_id, seq); 
+            
+            //Error exception for empty map
+            if(input_map == NULL){
+              ROS_ERROR_STREAM("map input is empty");
+              break;
+            }
+            
+            
+            
+              
+            
+        }
+
 
     
 
