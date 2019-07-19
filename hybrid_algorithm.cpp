9d2cef63443f88a27ea1719235026f7fb3c8a364
asdwer20 <asdwer20@gmail.com>
2019-07-20 07:35:11 +0900

    debugging in process

--- hybrid_algorithm.cpp
+++ hybrid_algorithm.cpp
@@ -1,11 +1,14 @@
-#include <ros/ros.h>
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <algorithm>
 
 #include "stdlib.h"
 
+const float pi = 3.1415926535897;
+
 //compares state to map to find if there is an obstacle
-std::bool valid_state_check(std::vector<std::vector<int>> map, std::vector<int> state){
+bool valid_state_check(std::vector<std::vector<int>> map, std::vector<int> state){
   if(map[state[1]][state[2]] == 1){
     return false;
   } else {
@@ -14,15 +17,15 @@ std::bool valid_state_check(std::vector<std::vector<int>> map, std::vector<int> 
 }
 
 //euclidean distance from state to goal
-float heuristic_distance(int x, int y, vector<int> goal){
-  distance = sqrt((goal[0]-x)^2 + (goal[1]-y)^2);
+double heuristic_distance(int x, int y, std::vector<int> goal){
+  double distance = sqrt((goal[0]-x)^2 + (goal[1]-y)^2);
   return distance;
 }
 
-//converts continuous (float) to discrete (int)
-std::vector<int> return_discrete(float x, float y){
-  x_dis = round(x);
-  y_dis = roound(y);
+//converts continuous (double) to discrete (int)
+std::vector<int> return_discrete(double x, double y){
+  int x_dis = round(x);
+  int y_dis = round(y);
 
   std::vector<int> discrete_coord = [x,y];
 
@@ -31,34 +34,34 @@ std::vector<int> return_discrete(float x, float y){
 
 //comparison function for the sort function below and compares
 //the first item of two vectors
-std::bool compare_vectors(const vector<float>& v1, const vector<float>& v2){
+bool compare_vectors(const std::vector<double>& v1, const std::vector<double>& v2){
   return v1[0] < v2[0];
 }
 
 //sort function for a vector of vectors
-std::vector<std::vector<float>> sort(std::vector<std::vector<float>> input){
+std::vector<std::vector<double>> sort_vectors(std::vector<std::vector<double>> input){
   std::sort(input.begin(), input.end(), compare_vectors);
   return input;
 }
 
 //print path found
-void print_path(std::vector<float> end, std::vector<float> start, std::vector<float> heading_changes, float drive_distance, std::vector<std::vector<float>> heading_map){
+void print_path(std::vector<double> end, std::vector<double> start, std::vector<double> heading_changes, double drive_distance, std::vector<std::vector<double>> heading_map){
   bool FINISHED = false;
-  std::vector<std::vector<float>> path = [];
+  std::vector<std::vector<double>> path = {};
 
   //start point
-  float x1 = end[2];
-  float y1 = end[3];
-  float theta1 = end[4];
-  float turn = end[5];
+  double x1 = end[2];
+  double y1 = end[3];
+  double theta1 = end[4];
+  double turn = end[5];
 
-  std::vector<float> state = [x1, y1, theta1, turn];
+  std::vector<double> state = {x1, y1, theta1, turn};
 
   path.push_back(state);
 
-  float x2 = 0;
-  float y2 = 0;
-  float theta2 = 0;
+  double x2 = 0;
+  double y2 = 0;
+  double theta2 = 0;
 
   while(FINISHED == false){
     x2 = x1 - drive_distance*cos(turn);
@@ -81,43 +84,46 @@ void print_path(std::vector<float> end, std::vector<float> start, std::vector<fl
       FINISHED = true;
     }
   }
-  cout << "the path found is: " << path;
+  std::cout << "the path found is: ";
+  
 }
+
+/* ACTUAL CODE STARTS FROM HERE */
 int main(){
   std::vector<std::vector<int>> map_input =
-  [ [0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 0, 0] ];
+  { {0, 0, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 1, 1, 1, 1, 1, 0, 0},
+    {0, 0, 1, 1, 1, 1, 1, 1, 0, 0},
+    {0, 0, 1, 1, 1, 1, 1, 1, 0, 0},
+    {0, 0, 1, 1, 1, 1, 1, 1, 0, 0} };
 
-  std::vector<std::vector<float>> heading_change_map = map_input;
+  std::vector<std::vector<int>> heading_change_map = map_input;
   //define goal states
   //[x, y, theta (in rad)]
-  std::vector<int> start_state = [0, 0, 0];
-  std::vector<int> goal_state = [map_input.size() - 1, map_input.size()-1, -pi/2];
-  std::vector<std::vector<int>> path = [];
+  std::vector<int> start_state = {0, 0, 0};
+  std::vector<float> goal_state = {9, 9, -pi/2};
+  std::vector<std::vector<int>> path = {};
 
-  std::bool PATH_FOUND = false;
-  std::bool NO_PATH = false;
+  bool PATH_FOUND = false;
+  bool NO_PATH = false;
 
   //because we do not know the wheel base calculation, the max steering angle
   //has not been placed under consideration
-  float drive_distance = sqrt(2)+0.1;
-  float max_steering_angle = pi/4;
-  std::vector<float> heading_changes = [pi/4, 0, -pi/4];
+  double drive_distance = sqrt(2)+0.1;
+  double max_steering_angle = pi/4;
+  std::vector<double> heading_changes = {pi/4, 0, -pi/4};
   int expansion = 0;
 
   //state vector definition: [cost, expansion number, x, y, orientation, orientation from previous node]
-  std::vector<float> current_state = [0, 0, start_state[0], start_state[1], start_state[2], 0];
+  std::vector<int> current_state = {0, 0, start_state[0], start_state[1], start_state[2], 0};
 
-  std::vector<std::vector<int>> open = [];
-  std::vector<std::vector<int>> closed = [];
+  std::vector<std::vector<int>> open = {};
+  std::vector<std::vector<int>> closed = {};
   open.push_back(current_state);
 
   while(open.empty()== false){
@@ -137,16 +143,16 @@ int main(){
       break;
     }
     for(int i = 0; i < heading_changes.size();i++ ){
-      float current_x = current_state[2];
-      float current_y = current_state[3];
-      float current_theta = current_state[4];
+      double current_x = current_state[2];
+      double current_y = current_state[3];
+      double current_theta = current_state[4];
 
       //calculate next state for this iteration of heading change
-      float next_x = current_x + drive_distance*cos(heading_changes[i]);
-      float next_y = current_y + drive_distance*sin(heading_changes[i]);
-      float next_theta = current_theta + heading_changes[i];
+      double next_x = current_x + drive_distance*cos(heading_changes[i]);
+      double next_y = current_y + drive_distance*sin(heading_changes[i]);
+      double next_theta = current_theta + heading_changes[i];
 
-      discrete_next = return_discrete(next_x, next_y).push_back(next_theta);
+      std::vector<int> discrete_next = return_discrete(next_x, next_y).push_back(next_theta);
 
       //if the state is valid, then calculate heuristics for the path, add to
       //the open vector
@@ -154,8 +160,8 @@ int main(){
         expansion++;
 
         //calculate cost of this new path using heuristics (changeable)
-        float heuristic = heuristic_distance(next_x, next_y, goal_state) + drive_distance;
-        std::vector<float> next_state = [heuristic, expansion, next_x, next_y, next_theta, i];
+        double heuristic = heuristic_distance(next_x, next_y, goal_state) + drive_distance;
+        std::vector<double> next_state = [heuristic, expansion, next_x, next_y, next_theta, i];
 
         open.push_back(next_state);
       }
