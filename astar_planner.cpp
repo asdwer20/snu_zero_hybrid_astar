91dc4f95e3d4569ad39819cebfb329b832daa89b
Ariadne <zeuskimone@gmail.com>
2019-07-24 21:33:46 +0900

    ns

--- astar_planner.cpp
+++ astar_planner.cpp
@@ -1,6 +1,8 @@
-#include <ros/ros.h>
+#include "ros/ros.h"
 #include <string>
 #include <iostream>
+#include <functional>
+#include <cmath>
 
 #include "ompl/base/spaces/ReedsSheppStateSpace.h"
 #include "ompl/geometric/SimpleSetup.h"
@@ -8,7 +10,9 @@
 #include "ompl/geometric/planner.h"
 #include "carsetupcomhandle.h"
 
-namespace ompl_geo = omple::geometric;
+
+namespace ob = ompl::base;
+namespace og = ompl::geometric;
 
 int main(int argc, char **argv){
     std::string node_name = "hybrid_astar_planner";
@@ -17,7 +21,7 @@ int main(int argc, char **argv){
         
     //Setting up StateSpace using the OMPL Library
     //The space is a Reeds Shepps State Space with a custom planner setup
-    ompl::base::StateSpacePtr state_space(new ompl::base::SE2StateSpace);
+    ob::StateSpacePtr state_space(new ob::SE2StateSpace);
     
     //Get map date from Nodehandle
     std::string map_id;
@@ -45,7 +49,7 @@ int main(int argc, char **argv){
             //Set dimensions and bounds for the input map
             map_length = input_map.height;
             map_width = input_map.width;
-            ompl::base::RealVectorBounds map_bounds(2);
+            ob::RealVectorBounds map_bounds(2);
             map_bounds.setLow(0, -map_length);
             map_bounds.setLow(1, -map_width);
             map_bounds.setHigh(0. map_length);
@@ -54,8 +58,8 @@ int main(int argc, char **argv){
             //Setup state validity checker using the isStateValid function within 
             //CarSetupComHandle header
             state_space_type.setBounds(map_bounds);
-            ompl::base::SpaceInformation *space_info = state_space.getSpaceInformation().get();
-            state_space.setStateValidityChecker([input_map, seq, space_info](const ompl::base::State *state)
+            ob::SpaceInformation *space_info = state_space.getSpaceInformation().get();
+            state_space.setStateValidityChecker([input_map, seq, space_info](const ob::State *state)
             
             
             
