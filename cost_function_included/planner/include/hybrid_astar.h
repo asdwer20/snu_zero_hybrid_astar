2328d9e5bb6baee3b09f9e7b6d62a9c19d84b9f3
Ariadne <zeuskimone@gmail.com>
2019-08-16 12:52:38 +0900

    rename

---
+++ cost_function_included/planner/include/hybrid_astar.h
@@ -0,0 +1,51 @@
+// hybrid_astar.h
+
+#ifndef HYBRIDASTAR
+#define HYBRIDASTAR
+
+#include <ompl/base/Planner.h>
+#include <ompl/control/SpaceInformation.h>
+#include <ompl/base/State.h>
+#include <vector>
+#include <cmath>
+
+
+namespace ompl {
+    
+    class hybridASTAR : public base::Planner {
+        public :
+
+        hybridASTAR(const base::SpaceInformationPtr &si);
+        ~hybridASTAR(void) override;
+        
+        base::PlannerStatus solve(const base::PlannerTerminationCondition &ptc) override;
+        
+        int return_lowest_cost_path(std::vector<double> input);
+       
+        double drive_distance = sqrt(2) + 0.1;
+        double cost;
+        double pi = 3.14159265;
+        
+        void clear(void) override;
+        void setup(void) override;
+        void freeMemory();
+
+        double euclidean_distance(base::State *state, base::State *goal);
+        double calculate_cost(base::State *start, base::State *goal, int turn_index);
+        
+        std::vector<double> return_discrete(double x, double y);
+        std::vector<double> heading_changes = {-pi/4, 0, pi/4};
+        std::vector<base::Path *> sort_vectors(std::vector<base::Path *> input);
+        
+        bool compare_path_costs(const base::Path &v1, const base::Path &v2);
+        bool vector_contains(std::vector<base::State *> input, base::State *item);
+        bool state_compare(base::State* input, base::State* goal);
+    
+        float heuristic;
+       
+        
+    };
+    typedef std::shared_ptr<hybridASTAR> hybridASTARPtr;
+}
+
+#endif
