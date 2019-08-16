62644ada850510a9cc7a3b9c5e7d7d701eb605f4
asdwer20 <asdwer20@gmail.com>
2019-08-16 14:14:15 +0900

    adding checkpoints

--- planner/src/hybrid_astar.cpp
+++ planner/src/hybrid_astar.cpp
@@ -34,6 +34,7 @@ namespace ompl{
       bool PATH_FOUND = false;        
       std::vector<base::Path *> open;
       std::vector<base::State *> closed;   
+      std::vector<double> cost_vector;
       //Debugging open and closed
       // std::cout << "-------------------debug-----------------" << std::endl;
       // std::cout << "size of open = " << open.size() << std::endl;
@@ -62,10 +63,10 @@ namespace ompl{
       double start_y = start->as<base::SE2StateSpace::StateType>()->getY();
       double start_theta = start->as<base::SE2StateSpace::StateType>()->getYaw();
 
-      // std::cout << "-------------------debug-----------------" << std::endl;
-      // std::cout << "start state:" << start_x << ", " << start_y << ", " << start_theta << std::endl;
-      // std::cout << "goal state:" << goal_x << ", " << goal_y << ", " << goal_theta << std::endl;
-      // std::cout << "----------------debug end-----------------" << std::endl;
+      std::cout << "-------------------debug-----------------" << std::endl;
+      std::cout << "start state:" << start_x << ", " << start_y << ", " << start_theta << std::endl;
+      std::cout << "goal state:" << goal_x << ", " << goal_y << ", " << goal_theta << std::endl;
+      std::cout << "----------------debug end-----------------" << std::endl;
 
 
       //The open and closed states are slightly different from before as they are now complete 
@@ -75,16 +76,17 @@ namespace ompl{
       base::Path *next_path;
       
       //Log appears not produce anything beyond this point
-      current_path->as<geometric::PathGeometric>()->append(start);
-      double l = current_path->length();
-      base::Cost path_cost = current_path->cost(obj);
-      double c = path_cost.value();
+      //current_path->as<geometric::PathGeometric>()->append(start); // This line doesn't work
+      //double l = current_path->length();
+      
+      double path_cost = calculate_cost(start, goal, 1);
+      cost_vector.push_back(path_cost);
       open.push_back(current_path);
       double s = open.size();
-      // std::cout << "-------------------debug-----------------" << std::endl; 
-      // std::cout << "size of current cost after appending start: " << l << std::endl;
+       std::cout << "-------------------debug-----------------" << std::endl; 
+       std::cout << "size of current cost after appending start: " << path_cost << std::endl;
       // std::cout << "cost of current path: " << c << std::endl;
-      // std::cout << "length of open after appending current path: "<< s << std::endl;
+       std::cout << "length of open after appending current path: "<< s << std::endl;
       base::State *current_state = start;
       base::State *discrete_state;
       base::State *next_state;
@@ -95,25 +97,34 @@ namespace ompl{
           ptc.terminate();
           break;
         } 
-        //open = sort_vectors(open); //WORKON:sort path based on the heuristic distance (currently not working properly) 
+         
         int index = 0;
-        index = return_lowest_cost_path(open, obj);
+        index = return_lowest_cost_path(cost_vector);
         current_path = open[index];
-        current_state = current_path->as<geometric::PathGeometric>()->getState(current_path->as<geometric::PathGeometric>()->getStateCount()-1);
+
+        //DEBUGGING
+        std::cout << "CHECKPOINT 1" << std::endl;
+        //current_state = current_path->as<geometric::PathGeometric>()->getState(current_path->as<geometric::PathGeometric>()->getStateCount()-1);
+        
         open.erase(open.begin() + index);
-       
+        cost_vector.erase(cost_vector.begin() + index);
         double current_x = current_state->as<base::SE2StateSpace::StateType>()->getX();
         double current_y = current_state->as<base::SE2StateSpace::StateType>()->getY();
         std::vector<double> disc_coord = return_discrete(current_x, current_y);
 
-        discrete_state->as<base::SE2StateSpace::StateType>()->setXY(disc_coord[0], disc_coord[1]);
-
+        //ANOTHER PROBLEM LINE
+        //discrete_state->as<base::SE2StateSpace::StateType>()->setXY(disc_coord[0], disc_coord[1]); //DOESNT WORK
+        
         closed.push_back(discrete_state);
 
+        //DEBUGGING
+        std::cout << "CHECKPOINT 2" << std::endl;
+        
         //Condition of the current state examined is the goal state
         if(state_compare(current_state, goal)){ 
           std::cout << "path found" << std::endl;
           current_path->print(std::cout);
+          std::cout << "CHECKPOINT FIN" << std::endl;
           break;
         }
         for(int i = 0; i < heading_changes.size()-1; i++){
@@ -121,15 +132,24 @@ namespace ompl{
             current_state->as<base::SE2StateSpace::StateType>()->getX() + 
             drive_distance*cos(current_state->as<base::SE2StateSpace::StateType>()->getYaw())
           );
+          std::cout << "CHECKPOINT 3.0" << std::endl;
+          next_state->as<base::SE2StateSpace::StateType>()->setY(
+            current_state->as<base::SE2StateSpace::StateType>()->getY() + 
+            drive_distance*sin(current_state->as<base::SE2StateSpace::StateType>()->getYaw())
+          );
+          std::cout << "CHECKPOINT 3.5" << std::endl;
           next_state->as<base::SE2StateSpace::StateType>()->setYaw(
             current_state->as<base::SE2StateSpace::StateType>()->getYaw()+heading_changes[i]
           );
-        }
-        if(si_->isValid(next_state) && vector_contains(closed, next_state) == false){
-          heuristic = euclidean_distance(next_state, goal) + drive_distance;
-          next_path = current_path; 
-          next_path->as<geometric::PathGeometric>()->append(next_state); 
-          open.push_back(next_path);
+          
+          if(si_->isValid(next_state) && vector_contains(closed, next_state) == false){
+            cost = calculate_cost(next_state, goal, i);
+            next_path = current_path; 
+            std::cout << "CHECKPOINT 4" << std::endl;
+            next_path->as<geometric::PathGeometric>()->append(next_state); 
+            open.push_back(next_path);
+            cost_vector.push_back(cost);
+          }
         }
       }
     }
@@ -165,26 +185,17 @@ namespace ompl{
       return FOUND;
     }
 
-    // bool compare_path_costs(const base::Path &v1, const base::Path &v2) {
-    //   double c1 = v1.as<geometric::PathGeometric>()->cost(obj)->value();
-    //   double c2 = v2.as<geometric::PathGeometric>()->cost(obj)->value();
-
-    //   return c1 > c2; 
-    // }
-
-    // std::vector<base::Path *> hybridASTAR::sort_vectors(std::vector<base::Path *> input){
-    //     std::sort(input.begin(), input.end(), compare_path_costs);
-    //     return input;
-    // }
-
-    int hybridASTAR::return_lowest_cost_path(std::vector<base::Path*> input, base::OptimizationObjectivePtr obj){
-      int index;
+    int hybridASTAR::return_lowest_cost_path(std::vector<double> input){
+      int index = 0;
+      double lowest_cost;
       for(int i = 0; i < input.size(); i++){
         if(i == 0){
           index = i;
+          lowest_cost = input[i];
         } else {
-          if(input[i]->cost(obj).value() < input[index]->cost(obj).value()){
+          if(input[i] < lowest_cost){
             index = i;
+            lowest_cost = input[i];
           }
         }
       }
@@ -206,6 +217,16 @@ namespace ompl{
         return false;
       }
     }
+
+    double hybridASTAR::calculate_cost(base::State *start, base::State *goal, int turn_index){
+      double distance = euclidean_distance(start, goal);
+      double turn_weight = 10;
+      if(turn_index != 1){
+        distance = distance + turn_weight;
+      }
+      return distance + drive_distance;
+    }
+
     void hybridASTAR::setup(void) {
         Planner::setup();
         //mtree_.freeMemory();
