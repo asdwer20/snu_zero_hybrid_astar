37fdf7e6916223011a15859d8e783ba6ba7c7b91
ariadne240 <limcos2x@gmail.com>
2019-08-26 21:04:39 +0900

    update

--- bi_working/planner/include/binomial_heap.h
+++ bi_working/planner/include/binomial_heap.h
@@ -7,4 +7,16 @@
 #include <iostream>
 #include <list>
 
+#include "costpath.h"
+
+using namespace std;
+
+struct Node
+{
+    int degree;
+    Costpath data;
+    Node *child, *sibling, *parent;
+};
+
+
 #endif
